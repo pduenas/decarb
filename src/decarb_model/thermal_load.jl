e6d07d4ff6ab750961c1f88c6283f338f7fd0df9
pduenas <pduenas@mit.edu>
2026-08-17 21:35:51 +0100

    Update thermal_load.jl
    Remove commented lines related to old 2RC model

--- src/decarb_model/thermal_load.jl
+++ src/decarb_model/thermal_load.jl
@@ -37,9 +37,6 @@ function thermal_load!(model::Model,in::Dict,tm::Dict,bdg::Dict,topo::Dict,chp::
     # maximum indoor temperature [°C]
     @constraint(model, eTmx[t=1:tm["P"]; tm["Ton"][t]==1], vTin[t]-vTup[t] <= tm["Tmx"][t])
 
-    # Au = calculate_conductivity_ventilation(bdg["Balt"],bdg["Bventy"],bdg["Bvent"],tm["P"],
-    #     tm["Tout"],tm["Bppl"])
-
     # thermal gain from active equipment [kWh]
     @expression(model, vQ_HTAC[t=1:tm["P"]],
         sum(model[:vCHP_HT][t,c] for c=1:chp["N"] if (!iszero).(topo["chp_bdg"][c,1])) + 
@@ -52,37 +49,10 @@ function thermal_load!(model::Model,in::Dict,tm::Dict,bdg::Dict,topo::Dict,chp::
     # calculate total radiation on roof and external facades
     Q_R = tm["Q_R"]
 
-    # # calculate capacitance of RC model
-    # B_C = bdg["Bfoot"]*bdg["Bslab"]*bdg["Btslab"]*bdg["Bpslab"]*bdg["Bcpslab"]
-    # # calculate resistance '1' of RC model
-    # B_R1 = 1/bdg["Bfoot"]/bdg["Bslab"]/bdg["Bkslab"]
-    # # calculate resistance '2' of RC model
-    # B_R2 = 1/sum(bdg["Bwall"].*bdg["Bkwall"],dims=1)[1]
-    # B_R2 = 1 ./ (1/B_R2.+Au)    # correct resistance due to ventilation
-    # replace!(B_R2,Inf=>1)
-
-    # # temperature balance [°C]
-    # if B_C>0.01     # mass in floor slabs
-    #     @constraint(model, eTbal0,      # initial period
-    #         vQ_HTAC[1]+Q_IHG[1] == -1/B_R2[1]*tm["Tout"][1] +
-    #         (B_R1+B_R2[1])/(B_R1*B_R2[1])*(vTin[1]-in["Tin0"]-(tm["TM"][1]/B_C*Q_R[1])))
-    #     @constraint(model, eTbal[t=2:tm["P"]],
-    #         vQ_HTAC[t]+Q_IHG[t] == 
-    #         (1-tm["TM"][t]/(B_R1*B_C))*(vQ_HTAC[t-1]+Q_IHG[t-1]) -
-    #         1/B_R2[t]*(tm["Tout"][t]-tm["Tout"][t-1]) +
-    #         (B_R1+B_R2[t])/(B_R1*B_R2[t])*(vTin[t]-vTin[t-1]-(tm["TM"][t]/B_C*Q_R[t])) +
-    #         tm["TM"][t]/(B_R1*B_R2[t]*B_C)*(vTin[t-1]-tm["Tout"][t-1]))
-    # else            # mass in walls
-    #     @constraint(model, eTbal0,      # initial period
-    #         vQ_HTAC[1]+Q_IHG[1]== (1/B_R1)*(vTin[1]-in["Tin0"]) - 
-    #         tm["TM"][1]/(B_R1*B_C)*Q_R[1])
-    #     @constraint(model, eTbal[t=2:tm["P"]],
-    #         vQ_HTAC[t]+Q_IHG[t] == 
-    #         (1-tm["TM"][t]*B_R1*B_R2[t]/(B_R1*B_R2[t]*B_C))*(vQ_HTAC[t-1]+Q_IHG[t-1]) +
-    #         tm["TM"][t]/(B_R1*B_R2[t]*B_C)*(vTin[t-1]-tm["Tout"][t-1]) +
-    #         (1/B_R1)*(vTin[t]-vTin[t-1]) - tm["TM"][t]/(B_R1*B_C)*Q_R[t])
-    # end
-
+    # thermal model based on data-driven parameters:
+    #   1- indoor to outdoor temperature difference
+    #   2- solar radiation on building envelope
+    #   3- heating/cooling balance and internal heat gains
     @constraint(model, eTbal0,      # initial period
         vTin[1] == 
         in["Tin0"] + bdg["Bk1"]*(tm["Tout"][1]-in["Tin0"]) + bdg["Bk2"]*Q_R[1] + bdg["Bk3"]*(vQ_HTAC[1]+Q_IHG[1]))
@@ -103,41 +73,3 @@ function thermal_load!(model::Model,in::Dict,tm::Dict,bdg::Dict,topo::Dict,chp::
         tm["HWdem"][t]*(1-vNShw[t]))
 
 end
-
-
-# function calculate_conductivity_ventilation(alt,venty,vent,nP,Tout,ppl)
-
-#     # air density variation with temperature [kg/m3]
-#     Ap_T = [1.4224,1.3943,1.3673,1.3413,1.3163,1.2922,1.269,1.2466,1.225,1.2041,1.1839,1.1644,1.1455]
-#     A_T  = [   -25,   -20,   -15,   -10,    -5,     0,    5,    10,   15,    20,    25,    30,    35]
-
-#     # air density correction by altitude [kg/m3]
-#     Ap_T = Ap_T.*exp(-alt/10400)
-
-#     # air volumetric heat capacity [kWh/m3-°C]
-#     Avhc_T = Ap_T.*0.0002793
-
-#     # conductivity due to ventilation per person [kW/°C/person]
-#     Au_T = Avhc_T.*vent
-#     Au = Array{Float64}(undef,nP)       # initialize conductivity vector
-#     for t=1:nP
-#         lb = diff([Tout[t].>=A_T;0],dims=1).!=0     # lower bound for interpolation
-#         ub = diff([0;Tout[t].<=A_T],dims=1).!=0     # upper bound for interpolation
-#         # identify temperature interval for interpolation
-#         xin = (1:length(A_T)).*(lb.|ub)
-#         xin_T = Array{Float64}(undef,sum(xin.!=0))
-#         xin_T = xin[xin.!=0]
-#         # if coincident upper and lower bound, no need for interpolation
-#         length(xin_T)==1 ? Au[t]=Au_T[xin_T[1]] : 
-#         # else, interpolate within interval of temperatures
-#             Au[t]=(Au_T[xin_T[2]]-Au_T[xin_T[1]])/(A_T[xin_T[2]]-A_T[xin_T[1]])*(Tout[t]-A_T[xin_T[1]])+Au_T[xin_T[1]]
-#     end
-
-#     # conductivity due to ventilation [kW/°C]
-#     if venty==0         # forced ventilation varies with level of occupancy
-#         return Au.*ppl
-#     elseif venty==1     # forced ventilation fixed at maximum occupancy
-#         return Au.*maximum(ppl)
-#     end
-
-# end
