f1dbf195c20d9c1f91c8093adfa6ceaed3a77440
pduenas <pduenas@mit.edu>
2026-09-12 08:53:18 +0200

    Removed supplemental firing
    Simplified more realistic topology for heat connection

--- src/case_runner/case_runner.jl
+++ src/case_runner/case_runner.jl
@@ -156,7 +156,7 @@ function run_decarb!(path::AbstractString,mip_gap::Float64,time_limit::Float64,i
     df_wind = read_der(model,sp,"wind",wind,tm["Date"],tm["IW"])
     df_elec,balance = read_electric(model,tm,chp["N"],hvac["N"],wh["N"],pv["N"],
         bess["N"],ev["N"],wind["N"],bess["mx"],ev["mx_eff"],collect(wh["fuel"]))
-    df_fuel = read_fuel(model,tm,chp["N"],abp["N"],wh["N"],topo["N"])
+    df_fuel = read_fuel(model,tm,chp["N"],abp["N"],wh["N"])
     df_indoor = read_indoor(model,tm,chp["N"],abp["N"],hvac["N"],wh["N"],wh["tank"],
         topo["chp_bdg"])
     df_dual,df_econ = read_econ(model,tm)
