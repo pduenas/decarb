0a671ee5d9933d42a7f8548f131ec3f620cc812d
pduenas <pduenas@mit.edu>
2026-09-15 10:22:42 +0200

    Improved thermal formulation

--- src/decarb_model/thermal_load.jl
+++ src/decarb_model/thermal_load.jl
@@ -39,8 +39,8 @@ function thermal_load!(model::Model,cfg::Dict,tm::Dict,bdg::Dict,topo::Dict,chp:
 
     # thermal gain from active equipment [kWh]
     @expression(model, vQ_HTAC[t=1:tm["P"]],
-        sum(model[:vCHP_HT][t,c] for c=1:chp["N"] if (!iszero).(topo["chp_bdg"][c,1])) + 
-        sum(model[:vHVAC_HTAC][t,h] for h=1:hvac["N"] if (hvac["HVmx_eff"][h]>0 || hvac["ACmx_eff"][h]>0)) - 
+        sum(model[:vCHP_HT][t,c] for c=1:chp["N"] if (!iszero).(topo["chp_bdg"][c,1])) +
+        sum(model[:vHVAC_HTAC][t,h] for h=1:hvac["N"] if (hvac["HVmx_eff"][h]>0 || hvac["ACmx_eff"][h]>0)) -
         sum(model[:vABP_AC][t,a] for a=1:abp["N"]))
 
     # calculate internal heat gains from occupancy, lighting and electrical equipment
@@ -54,12 +54,12 @@ function thermal_load!(model::Model,cfg::Dict,tm::Dict,bdg::Dict,topo::Dict,chp:
     #   2- solar radiation on building envelope
     #   3- heating/cooling balance and internal heat gains
     @constraint(model, eTbal0,      # initial period
-        vTin[1] == 
+        vTin[1] ==
         cfg["Tin0"] + bdg["Bk1"]*(tm["Tout"][1]-cfg["Tin0"]) + bdg["Bk2"]*Q_R[1] + bdg["Bk3"]*(vQ_HTAC[1]+Q_IHG[1]))
     @constraint(model, eTbal[t=2:tm["P"]],
-        vTin[t] == 
-        vTin[t-1] + bdg["Bk1"]*(tm["Tout"][t-1]-vTin[t-1]) + bdg["Bk2"]*Q_R[t] + bdg["Bk3"]*(vQ_HTAC[t]+Q_IHG[t]))
-    
+        vTin[t] ==
+        vTin[t-1] + bdg["Bk1"]*(tm["Tout"][t]-vTin[t-1]) + bdg["Bk2"]*Q_R[t] + bdg["Bk3"]*(vQ_HTAC[t]+Q_IHG[t]))
+
     # non-served hot water [0,1]
     @variable(model, 1 >= vNShw[t=1:tm["P"]] >= 0)
 
@@ -67,9 +67,9 @@ function thermal_load!(model::Model,cfg::Dict,tm::Dict,bdg::Dict,topo::Dict,chp:
     cfg["NSHWcost"]==0 ? fix.(vNShw,0; force=true) : println("   \u2139  Discomfort hot water allowed")
 
     # domestic hot water balance [kWh]
-    @constraint(model, eHWbal[t=1:tm["P"]], 
-        sum(model[:vWH_HW][t,w] for w=1:wh["N"] if wh["mx_eff"][w]>0) + 
-        sum(model[:vCHP_HW][t,c] for c=1:chp["N"] if (!iszero).(topo["chp_bdg"][c,2])) == 
+    @constraint(model, eHWbal[t=1:tm["P"]],
+        sum(model[:vWH_HW][t,w] for w=1:wh["N"] if wh["mx_eff"][w]>0) +
+        sum(model[:vCHP_HW][t,c] for c=1:chp["N"] if (!iszero).(topo["chp_bdg"][c,2])) ==
         tm["HWdem"][t]*(1-vNShw[t]))
 
 end
